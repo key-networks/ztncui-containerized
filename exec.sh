cd8f8ddd7297cb1f54769cc8436e5027fa57c5e9
Key Networks <34238649+key-networks@users.noreply.github.com>
2018-09-29 21:58:55 +0800

    The existence of $HTTP_ALL_INTERFACES is tested, not the value

--- exec.sh
+++ exec.sh
@@ -2,7 +2,7 @@
 
 HTTP_PORT=${HTTP_PORT:-3000}
 HTTPS_PORT=${HTTPS_PORT:-3443}
-HTTP_ALL_INTERFACES=${HTTP_ALL_INTERFACES:-no}
+HTTP_ALL_INTERFACES=${HTTP_ALL_INTERFACES}
 
 /usr/sbin/zerotier-one &
 
@@ -15,6 +15,6 @@ cd /opt/key-networks/ztncui
 
 echo "HTTPS_PORT=$HTTPS_PORT" > /opt/key-networks/ztncui/.env
 echo "HTTP_PORT=$HTTP_PORT" >> /opt/key-networks/ztncui/.env
-echo "HTTP_ALL_INTERFACES=$HTTP_ALL_INTERFACES" >> /opt/key-networks/ztncui/.env
+[ ! -z $HTTP_ALL_INTERFACES ] && echo "HTTP_ALL_INTERFACES=$HTTP_ALL_INTERFACES" >> /opt/key-networks/ztncui/.env
 
 exec sudo -u ztncui /opt/key-networks/ztncui/ztncui
